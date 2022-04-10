d5c411ec051421d28185938fd135a8d63b825ef9
uday2925 <udaykiran1139@gmail.com>
2022-04-10 13:11:52 +0530

    completed assignment

---
+++ day3_counter_25133/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import Counter from './components/counter';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Counter value={0}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ day3_counter_25133/src/components/counter.jsx
@@ -0,0 +1,35 @@
+import "./counter.css";
+
+const { useState } = require("react");
+
+function Counter({value})
+{
+    
+    const [counter,setCounter]=useState(value);
+    //console.log({counter});    
+    function handlechange(values)
+    {
+        setCounter(counter+values);
+    }
+    function doublefunction()
+    {
+       setCounter(counter*2);
+    }
+
+
+    return(
+        <div>
+             
+            <p className={counter%2===0?"greentxt":"redtxt"}>counter:{counter}</p>
+            <button onClick={()=>{handlechange(1)}}>Increment</button>
+            <button onClick={()=>{handlechange(-1)}}>decrement</button>
+            <button onClick={()=>{doublefunction()}}>Double</button>
+        </div>
+    )
+    
+}
+
+export default Counter
+
+
+
